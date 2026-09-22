d64f4e870cac5110bdbe28562a58f999ac967d2f
siyacoder01 <siyasaji2007@gmail.com>
2026-09-22 21:54:21 +0530

    made changes

---
+++ variable.py
@@ -0,0 +1,36 @@
+# arthematic
+a=1
+b=6
+c=9
+d="siya"
+
+print(a+b)
+# @siya=90 not valid
+# print(c+d) not possible
+
+# assignment
+a=4-1
+print(a)
+b=6
+b+=3
+print(b)
+
+# comparison
+e=9>0
+print(e)
+
+#type 
+a="siya"
+t=type(a)
+print(t)
+
+g=input("enter number 1 = ")
+f=input("enter number 2 = ")
+i=g + f
+print("string =", i)
+
+g=int(input("enter number 1 = "))
+f=int(input("enter number 2 = "))
+i=g + f
+print("int =" , i)
+
